714e941ec24961178fb2b087844f48726fc178ea
Willy Biti <willy.biti@gmail.com>
2021-12-28 15:16:29 -0300

    esconder form

--- formulario/script.js
+++ formulario/script.js
@@ -5,6 +5,57 @@ function cadastrarPessoas(){    //função de acionar o botão cadastrar
     var telefone = document.getElementById('telefone').value;   //pegando ID no html
     var endereco = document.getElementById('endereco').value;  //pegando ID no html
     
+    //validação/verificação
+
+    if(nome == ""){
+        alert("Por favor digite seu nome")
+        document.getElementById("nome").focus();
+        return;
+    }
+    if(sobreNome == ""){
+        alert("Por favor digite seu sobrnome")
+        document.getElementById("sobrenome").focus();
+        return;
+    }
+    if(cpf == ""){
+        alert("Por favor digite seu cpf")
+        document.getElementById("cpf").focus();
+        return;
+    }
+    if(telefone == ""){
+        alert("Por favor digite seu telefone")
+        document.getElementById("telefone").focus();
+        return;
+    }
+    if(endereco == ""){
+        alert("Por favor digite seu endereço")
+        document.getElementById("endereco").focus();
+        return;
+    }
+
+
+    //Segundo tipo de validadção
+    /*
+    var mensagem = "";
+    if(nome == ""){
+        mensagem += " O nome\n"
+    }
+    if(cpf == ""){
+        mensagem += "O cpf\n"
+    }
+    if(telefone == ""){
+        mensagem += "O telefone\n"
+    }
+    if(endereco == ""){
+        mensagem += "O endereço\n"
+    }
+
+    if(mensagem != ""){
+        alert("prencher : \n " + mensagem)
+        return;
+    }
+    */
+
     var html = "<tr>";     //variável de pega a tableba no html
     html += "<td>"+ nome +"</td>";
     html += "<td>"+ sobreNome +"</td>";
@@ -21,7 +72,16 @@ function cadastrarPessoas(){    //função de acionar o botão cadastrar
     document.getElementById('telefone').value  = ""; 
     document.getElementById('endereco').value  = "";
 
-    alert("Cadastrado com sucesso")
+    // alert("Cadastrado com sucesso")
+    // document.getElementById('nome').focus();
 
-    document.getElementById('nome').focus();
+    document.getElementById('formCadastro').style.display = "none";
+    document.getElementById('mostrarForm').style.display = "block";
+}
+
+
+// função para minstrar e esconder formuláriodepois de cadastrar
+function mostarFormulario(){
+    document.getElementById('mostrarForm').style.display = "none";
+    document.getElementById('formCadastro').style.display = "block";
 }
